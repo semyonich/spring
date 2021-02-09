16e6d808ad4b8e4e90c34ee026e30745af0075a8
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-02-09 16:52:28 +0200

    warning fixed

--- src/main/java/com/spring/config/AppConfig.java
+++ src/main/java/com/spring/config/AppConfig.java
@@ -18,19 +18,19 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
         "com.spring.dao"
 })
 public class AppConfig {
-    private final Environment env;
+    private final Environment environment;
 
     public AppConfig(Environment env) {
-        this.env = env;
+        this.environment = env;
     }
 
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("db.driver"));
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.user"));
-        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.user"));
+        dataSource.setPassword(environment.getProperty("db.password"));
         return dataSource;
     }
 
@@ -39,8 +39,8 @@ public class AppConfig {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
         Properties props = new Properties();
-        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        props.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(props);
         factoryBean.setAnnotatedClasses(User.class);
         return factoryBean;
